fbb1891a8d6683697b6b68b98d3b79222bf17842
Tropic.Wang <Nemowang24@gmail.com>
2024-12-04 15:29:03 -0500

    change to use openai as llm

--- IntelligenteBookSearch.py
+++ IntelligenteBookSearch.py
@@ -7,51 +7,78 @@
 #   llama-index-llms-ollama
 
 import os
-import textwrap
-from pprint import pprint
-
-# from llama_index.llms.groq import Groq
-from llama_index.core import Settings
+import gradio as gr
 from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
-from llama_index.core.query_engine import RouterQueryEngine
-from llama_index.core.tools import QueryEngineTool, ToolMetadata
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+# from langchain.embeddings import HuggingFaceBgeEmbeddings
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 from llama_index.llms.ollama import Ollama
+import json
+from pprint import pprint
+from llama_index.core.tools import QueryEngineTool, ToolMetadata
+from llama_index.core.query_engine import RouterQueryEngine
+# from llama_index.llms.groq import Groq
+from groq import Groq
+from llama_index.core import Settings
+import textwrap
+# from openai import OpenAI
+from openai import ChatCompletion
+# import openai
+from llama_index.llms.openai import OpenAI
 
 os.chdir(r"D:\MyDrive2\pythonprojects\class\GENAI\LocalAILibrarian")
-os.environ["PYTHONPATH"] = r"D:\MyDrive2\pythonprojects\ailib;" + os.environ.get("PYTHONPATH", "")
+# os.environ['TRANSFORMERS_CACHE'] = r'C:\Users\tropi\.cache\huggingface\hub'
 book_path = "books"
 # GROQ_API_KEY = os.environ["GROQ_API_KEY"]
-# llm = Groq(api_key=GROQ_API_KEY)
+# llm_global = Groq(api_key=GROQ_API_KEY)
+# llm_global = Ollama(model="phi3.5:latest", base_url="http://localhost:11434")
+openai_api_key = os.getenv("OPENAI_API_KEY")
+# llm_global = OpenAI(
+#     # model_name="gpt-3.5-turbo",
+#     base_url="",
+#     api_key=openai_api_key
+# )
 
-# from sentence_transformers import SentenceTransformer
-# model = SentenceTransformer('BAAI/bge-small-en-v1.5')
+# llm_global = ChatCompletion(
+#     model='gpt-3.5-turbo',
+#     api_key=openai_api_key
+# )
 
-# Function to check if model exists before initializing
-# def check_and_initialize_embeddings(model_name):
-#     model_path = os.path.expanduser(f"~\\.cache\\huggingface\\{model_name}")
-#
-#     # Check if the directory exists
-#     if not os.path.exists(model_path):
-#         print(f"Model path '{model_path}' does not exist. Please check the model name or ensure it is downloaded.")
-#         return None
-#
-#     # Initialize embedding model
-#     return HuggingFaceEmbedding(model_name=model_name)
+llm_global = OpenAI(
+    model="gpt-4o-mini",
+    # api_key="some key",  # uses OPENAI_API_KEY env var by default
+)
+# client = OpenAI(
+#     # model_name="gpt-3.5-turbo",
+#     api_key=openai_api_key
+# )
 
-llm_global = Ollama(model="phi3.5:latest", base_url="http://localhost:11434")
-embed_model_global = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5", cache_folder=r"C:\Users\tropi\.cache\huggingface\hub")
-# embed_model_global = check_and_initialize_embeddings("BAAI/bge-small-en-v1.5")
-# if embed_model_global:
-#     Settings.llm = llm_global
-#     Settings.embed_model = embed_model_global
-# else:
-#     print("Failed to initialize embedding model due to missing files.")
+# llm_global = ChatCompletion(
+#     model='gpt-3.5-turbo',
+#     api_key=openai_api_key
+# )
+
+# llm_global = openai_model.Completion.create(model='gpt-3.5-turbo')
+# llm_global = client.completions.create(model='gpt-3.5-turbo')
+# llm_global = LLamaIndexOpenAI(
+#     api_key=openai_api_key,
+#     model_name="gpt-4o"  # Ensure this is the correct parameter for llama_index integration
+# )
+
+# embed_model_global = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+encode_kwargs = {'normalize_embeddings': True} # set True to compute cosine similarity
+embed_model_global = HuggingFaceBgeEmbeddings(
+    model_name="BAAI/bge-small-en-v1.5",
+    # model_kwargs=model_kwargs,
+    encode_kwargs=encode_kwargs,
+    query_instruction="For this sentence, generate notation for use in retrieving related articles:"
+)
+# embed_model_global.query_instruction = "For this sentence, generate notation for use in retrieving related articles:"
 
 Settings.llm = llm_global
 Settings.embed_model = embed_model_global
 
-
+# C:\Users\tropi\.cache\huggingface\hub\models--BAAI--bge-small-en-v1.5
 
 class BookIndexer:
     def __init__(self,book_path):
@@ -134,3 +161,23 @@ def search(query):
 
 
 pprint(search("Tell me about the specific details about Alan Turing's cryptographic methods at Bletchley Park"))
+
+
+# def greet(fns:str,dt):
+#     return f"Hello, {fns}, good {dt}!"
+#
+# with gr.Blocks() as demo:
+#     gr.Markdown("# Greeting App")
+#     output = gr.Textbox(label="Greeting")
+#
+#     with gr.Row():
+#         first_name = gr.Textbox(label="Enter your first_name", placeholder="input your first name")
+#         last_name = gr.Textbox(label="Enter your last_name", placeholder="input your last name")
+#         afternoon = gr.Textbox(label="Enter the time", placeholder="input morning or afternoon")
+#
+#     fns = gr.Textbox(str(first_name.value) + str(last_name.value)+"ABC")
+#
+#     greet_btn = gr.Button("Greet")
+#     greet_btn.click(fn=greet, inputs=(fns, afternoon),outputs=output)
+#
+# demo.launch()
